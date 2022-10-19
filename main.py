9f90f71639eb84663c7ea2daad5f0184a646f3a8
carlmenke <carlmenke13@gmail.com>
2022-10-19 14:35:12 -0700

    added simple control flow with python

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello welcome to your adventure")
+
+age = input('how old are you?')
+
+if int(age) < 12:
+    print('Sorry you are not old enough to continue')
+    exit()
+else: print('You are allowed to continue')
